37cf2a108df745395f4cb73cd9e637ff70e21b6c
Givaldomacedo <givaldomacedo1501@gmail.com>
2025-07-29 00:00:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('form.html')  # nome correto do arquivo HTML
+def connect_db():
+    return mysql.connector.connect(
+        host=os.getenv("turntable.proxy.rlwy.net"),
+        user=os.getenv("root"),
+        password=os.getenv("EFETjqtkSYzvRUyZVRkieIMvDXSZcWMa"),
+        database=os.getenv("railway")
+    )
 
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        data = request.form['data']
+        historico = request.form['historico']
+        valor = request.form['valor']
+        conta = request.form['conta']
+
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO lancamentos (data, historico, valor, conta)
+            VALUES (%s, %s, %s, %s)
+        """, (data, historico, valor, conta))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect('/')
+
+    return render_template('form.html')
